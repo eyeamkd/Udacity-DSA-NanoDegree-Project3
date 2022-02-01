ad6a4d02a0fd33dba8e33a7a458efee71285516c
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-02 00:40:38 +0530

    tries completed

--- Tries/TrieClass.py
+++ Tries/TrieClass.py
@@ -1,4 +1,5 @@
 
+from asyncio.windows_events import NULL
 from mimetypes import init
 
 
@@ -34,4 +35,49 @@ trie = Trie()
 trie.add('Kunal')
 trie.add('Dubey')
 
-print(trie.exists('Kunal'))
+print(trie.exists('Kunal')) 
+
+
+
+from collections import defaultdict 
+
+class NewTrieNode:
+    def __init__(self) -> None:
+        self.children = defaultdict(TrieNode)
+        self.word = False
+        
+class NewTrie:
+    def __init__(self) -> None:
+        self.root = NewTrieNode()
+    
+    def add(self,word:str)->None: 
+        base_node = self.root;
+        for letter in word: 
+            base_node.children[letter] = NewTrieNode() 
+            base_node = base_node.children[letter] 
+        base_node.word = True 
+    
+    def exists(self,word:str)->bool: 
+        base_node = self.root
+        for letter in word: 
+            if(letter in base_node.children): 
+                base_node = base_node.children[letter] 
+            else:
+                return False 
+        return base_node.word 
+    
+    def print_node(self)->None:
+        print(self.root) 
+        
+    def __repr__(self) -> str:
+        return self.root
+                
+            
+#test 
+new_trie = NewTrie() 
+
+new_trie.add("kunal")
+new_trie.add("dubey") 
+new_trie.print_node() 
+result = new_trie.exists("kunal") 
+print(result)
