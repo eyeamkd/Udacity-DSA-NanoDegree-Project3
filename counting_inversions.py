8d5c6b0df6c21be3ebe31cd797cf7873b1198217
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-09 01:06:25 +0530

    FUCKING COUNTING INVERSIONS

---
+++ counting_inversions.py
@@ -0,0 +1,23 @@
+def count_inversion_pairs(arr:list)->int:
+    # divide the array into two halves 
+    
+      pass 
+
+def divide_array(arr:list):
+    length = len(arr) 
+    if length == 1:
+        return arr 
+    mid = length//2 
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    divide_array(left_half)
+    divide_array(right_half) 
+     
+
+def merge(left_half:list,right_half:list)->list:
+    pass
+    
+    
+    
+    # while merging the left and the right half apply the logic 
+    
--- merge_sort.py
+++ merge_sort.py
@@ -1,14 +1,20 @@
-def merge_sort_divide(arr: list) -> list:
-    # divide routine 
+def merge_sort_divide(arr: list, pairs) -> list:
+    # divide routine  
     length = len(arr)
     if length == 1:
-        return arr
+        return arr,0
     half_index = length // 2
-    sorted_left_half = merge_sort_divide(arr[:half_index])
-    sorted_right_half = merge_sort_divide(arr[half_index:])
+    sorted_left_half, left_half_pairs = merge_sort_divide(arr[:half_index], pairs)
+    sorted_right_half, right_half_pairs = merge_sort_divide(arr[half_index:], pairs)
 
-    # merge divided half routine  
-    return merge(sorted_left_half, sorted_right_half)
+    # merge divided half routine    
+    
+    
+    merged_array, merged_pairs   = index_merge(sorted_left_half, sorted_right_half,0,0,pairs)  
+    
+    pairs = max(pairs, merged_pairs)
+    
+    return merged_array, pairs
 
 
 def copy_arr(source: list, destination: list) -> list:
@@ -26,14 +32,49 @@ def merge(arr1: list, arr2: list) -> list:
         elif len(arr2) == 0:
             merged_array = copy_arr(arr1, merged_array)
             return merged_array
-        elif arr1[0] < arr2[0]:
+        elif arr1[0] < arr2[0]: 
+            print("Count here")
             merged_array.append(arr1[0])
             arr1.remove(arr1[0])
-        else:
+        else: 
+            print("Count here")
             merged_array.append(arr2[0])
-            arr2.remove(arr2[0])
+            arr2.remove(arr2[0]) 
+
+def index_merge(arr1:list, arr2:list, arr1_index:int, arr2_index:int, pairs=0)-> list:
+    merged_array = [] 
+    arr1_length = len(arr1)
+    arr2_length = len(arr2)  
+    arr1_index_limit = arr1_length - 1
+    arr2_index_limit = arr2_length - 1
+    while arr1_index <= arr1_index_limit and arr2_index<= arr2_index_limit: 
+        print("Array and index are", arr2)
+        if(arr1[arr1_index]>arr2[arr2_index]): 
+            print("Greater values are ", arr1, arr2)
+            pairs+= len(arr1)-arr1_index+1 
+            print("Pairs is ", pairs)
+            merged_array.append(arr2[arr2_index]) 
+            arr2_index+=1 
+        else:
+            merged_array.append(arr1[arr1_index])
+            arr1_index+=1 
+    if(arr1_index == arr1_index_limit):
+        while arr1_index <= arr1_index_limit:
+            merged_array.append(arr1[arr1_index])
+            arr1_index+=1
+    else:
+        while arr2_index <= arr2_index_limit:
+            merged_array.append(arr2[arr2_index])
+            arr2_index+=1
+             
+    return  merged_array,pairs  
+    
+         
+            
+        
+    
 
 
 # test case for the merge
-result = merge_sort_divide([3, 6, 9, 1, 2, 4])
-print(result)
+result, pairs  = merge_sort_divide([7, 5, 3, 1],0)
+print("Pairs are ", pairs)
