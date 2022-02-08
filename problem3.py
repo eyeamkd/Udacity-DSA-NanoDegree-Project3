1e2535e8f4a9d79ea5e3f5efd92d937d3e17443b
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-08 19:17:39 +0530

    windows init

---
+++ problem3.py
@@ -0,0 +1,19 @@
+def rearrangeNumbers(arr:list)->list:
+    # input range in arr is [0,9]
+    # output range is [x,y]
+    # diff of digits between x and y shouldn't be more than 1
+    
+    
+    
+    # sort the numbers in the decreasing order 
+    # check the length and form two numbers  
+    return []; 
+
+
+def sortArray(arr:list)->list:
+    return []  
+
+def checkLengthAndDivide(arr:list):(int,int)={
+    
+}
+    
