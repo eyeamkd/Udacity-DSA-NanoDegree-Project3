cae07de3155a53ebf0400b4127a684da302807ae
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-11 22:36:24 +0530

    heapifying

--- Heaps/heap.py
+++ Heaps/heap.py
@@ -1,8 +1,40 @@
-class HeapNode:
-    def __init__(self, priority:int, value:int ) -> None:
-        self.prority = priority
-        self.value = value 
+class PriorityQueue:
+    def __init__(self) -> None: 
+        self.cbt= [] 
         pass 
-
-def buildHeap():
+    
+    def insert(self, value:int, priority:int): 
+        node = HeapNode(value, priority) 
+        self.cbt.append(node) 
+        self.heapify() 
+        pass 
+    
+    def remove():
+        pass 
+    
+    def front():
+        pass 
+    
+    def size():
+        pass 
+    
+    def is_empty():
+        pass 
+    
+    def heapify(self): 
+        if len(self.cbt)>=2:
+            for node in reversed(self.cbt):
+                element_index = self.cbt.index(node) 
+                prev = self.cbt[element_index-1]
+                if prev.priority < node.priority:
+                    temp = node
+                    node = prev 
+                    prev = temp 
+             
+class HeapNode:
+    def __init__(self, value, priority) -> None: 
+        self.value = value
+        self.priority = priority
+        pass 
+     
     
---
+++ heap_sort.py
@@ -0,0 +1,14 @@
+class Heap():
+    def __init__(self) -> None:
+        pass
+
+class HeapNode():
+    def __init__(self) -> None:
+        pass 
+    
+def heap_sort(arr:list)->list: 
+    # convert the array into a max_heap 
+    # pop out the elements from the max_heap into an array 
+    # re-arrange the elements of the array in order to sort them out
+    heap = []
+     
