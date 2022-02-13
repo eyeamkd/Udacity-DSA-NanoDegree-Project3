f8af7bc027f8a9e6e5b5085a31eb71d8282fa724
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-14 04:32:09 +0530

    problem 3 solved

--- problem2.py
+++ problem2.py
@@ -1,48 +1,72 @@
-# Check for an element in the rotated array 
-def checkSorted(arr:list,index:int)->bool:
-    if(index>=1):
-        if(arr[index-1]<=arr[index]<=arr[index+1]):
-            return True
-        else:
-            return False 
+def rotated_array_search(arr,value)->int: 
+    # arr = input[0]
+    # value = input[1]
+    # pivot_search   
+    pivot_index = pivot_search(arr,0,len(arr)-1) 
+    #print("Pivot index is", pivot_index)
+    if arr[pivot_index] == value:
+        return pivot_index
+    # print(pivot) 
+    if arr[0]<=value<=arr[pivot_index]:
+        return binary_search(arr,0,pivot_index,value)
     else:
-        if(arr[0]<=arr[index]):
-            return True
-        else:
-            return False
+       return binary_search(arr,pivot_index+1,len(arr)-1, value)
+    
+    # binary_search on the appropriate half  
+    # return target index 
+    pass
 
-    # find the pivot point 
-    # divide the array by the pivot point 
-    # binary search for the index
-    # finding pivot point 
-
-def binarySearchCheckSorted(arr:list,low:int,high:int)->int:
-    while(low<high):
-        mid = low+high//2  
-        if(checkSorted(arr,mid)):
-            binarySearchCheckSorted(arr,mid,high)
-            binarySearchCheckSorted(arr,low,mid) 
+def binary_search(arr, low, high, value): 
+    if low<=high:
+        mid = (low+high)//2
+        if arr[mid] == value:
+            return mid
         else:
-            return arr.index(mid) 
-    return -1 
-    ## condition to handle, what if the element is not present at all? 
+            if arr[mid] > value:
+                return binary_search(arr,low, mid-1, value)
+            else:
+                return binary_search(arr, mid+1, high, value)
+    return -1
+            
+    
 
-def binarySearchForIndex(arr:list,low:int,high:int,element:int)->int:  
-    while(low<high):
-        mid = low+high//2
-        if(arr[mid]==element):
-            return arr.index(mid) 
-        else:
-            binarySearchForIndex(arr,low,mid-1,element)
-            binarySearchForIndex(arr,mid+1,high)
+def pivot_search(arr:list, low, high)->int: 
+    if high == low:
+        #print("Pivot is", high)
+        return high    
+    if arr[low] > arr[high]:   
+        mid = (low+high)//2   
+        if arr[low] >= arr[mid]:
+            return pivot_search(arr,low,mid)
+        else: 
+            return pivot_search(arr,mid,high)
+    else:
+        return None 
+
+
+        
+# testing  
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
     return -1
 
-def findIndex(arr:list,element:int)->int:  
-    length = len(arr)
-    pivot = binarySearchCheckSorted(arr,0,length-1)   
-    if(pivot!=element):
-        index = binarySearchForIndex(arr,0,pivot-1,element)
-        if(index==-1):
-            index = binarySearchForIndex(arr,pivot+1,element) 
-        return index
-    return pivot
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10]) 
+
+#print(rotated_array_search([4,5,6,7,0,1,2],0))
+    
+    
--- problem3.py
+++ problem3.py
@@ -1,72 +1,25 @@
-def rotated_array_search(arr,value)->int: 
-    # arr = input[0]
-    # value = input[1]
-    # pivot_search   
-    pivot_index = pivot_search(arr,0,len(arr)-1) 
-    #print("Pivot index is", pivot_index)
-    if arr[pivot_index] == value:
-        return pivot_index
-    # print(pivot) 
-    if arr[0]<=value<=arr[pivot_index]:
-        return binary_search(arr,0,pivot_index,value)
-    else:
-       return binary_search(arr,pivot_index+1,len(arr)-1, value)
-    
-    # binary_search on the appropriate half  
-    # return target index 
-    pass
+from numpy import number
+from Practice.quick_sort import quick_sort_in_place
 
-def binary_search(arr, low, high, value): 
-    if low<=high:
-        mid = (low+high)//2
-        if arr[mid] == value:
-            return mid
+
+def rearrange_digits(input_list):
+    sorted_list = quick_sort_in_place(input_list)
+    number1=''
+    number2='' 
+    for i in sorted_list[::-1]:
+        if i%2==0:
+            number1+=str(i) 
         else:
-            if arr[mid] > value:
-                return binary_search(arr,low, mid-1, value)
-            else:
-                return binary_search(arr, mid+1, high, value)
-    return -1
-            
-    
-
-def pivot_search(arr:list, low, high)->int: 
-    if high == low:
-        #print("Pivot is", high)
-        return high    
-    if arr[low] > arr[high]:   
-        mid = (low+high)//2   
-        if arr[low] >= arr[mid]:
-            return pivot_search(arr,low,mid)
-        else: 
-            return pivot_search(arr,mid,high)
-    else:
-        return None 
-
-
-        
-# testing  
-
-def linear_search(input_list, number):
-    for index, element in enumerate(input_list):
-        if element == number:
-            return index
-    return -1
+            number2+=str(i) 
+    return [int(number1), int(number2)]
 
 def test_function(test_case):
-    input_list = test_case[0]
-    number = test_case[1]
-    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
         print("Pass")
     else:
         print("Fail")
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10]) 
-
-#print(rotated_array_search([4,5,6,7,0,1,2],0))
-    
-    
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
