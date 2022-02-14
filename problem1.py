2abf53fe1dc987b819bce091cad40fe155626971
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-14 05:35:36 +0530

    updated the solution for problem 1

--- problem1.py
+++ problem1.py
@@ -1,19 +1,34 @@
-def get_square_root(number:int): 
+def sqrt(number:int): 
     if( type(number) is not int): 
         raise ValueError("Only integer input supported") 
     if(number==1 or number ==0):
         return number
     low = 2
-    high = number
-    while(low<high):
-        mid = (low+high)//2 
-        prev_mid = mid-1 
-        if(prev_mid*prev_mid< number and mid*mid > number): 
-            if((number - prev_mid*prev_mid) < (mid*mid-number)):
-                return prev_mid 
-            else:
-                return mid
-        if(mid*mid >= number):
-            high = mid
-        else:
-            low = mid
+    high = number 
+    return binary_search(low, high, number)
+
+
+def binary_search(low, high,value): 
+    if low==high:
+        return low  
+    mid = (low+high )//2 
+    if mid*mid == value:
+        return mid 
+    if (mid-1)*(mid-1) < value:
+        if (mid+1)*(mid+1)>value:
+            return mid  
+    if mid*mid > value:
+      return binary_search(low, mid, value)
+    elif mid*mid < value:
+        return binary_search(mid, high, value)
+    
+    return -1
+        
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail") 
+
+# print(binary_search(2,16,16))
