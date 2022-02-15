b2dd9567454e4aec76d6c4abe7eb9e597d627528
Kunal Dubey <kunaldubey2297@gmail.com>
2022-02-15 21:50:36 +0530

    added test cases for problem4

--- problem4.py
+++ problem4.py
@@ -1,35 +1,40 @@
-def sort_012(arr:list)->list:
-    zero_pointer = 0
-    two_pointer = len(arr)-1 
-    current_pointer = 0 
-    
-    while(arr[two_pointer]==2):
-        two_pointer-=1  
-    while(arr[zero_pointer]==0):
-        zero_pointer+=1 
-         
-    current_pointer = zero_pointer
+def sort_012(arr:list)->list: 
+    if len(arr)>0 and type(arr) is list:
+        zero_pointer = 0
+        two_pointer = len(arr)-1 
+        current_pointer = 0 
         
-    def swap(index_1, index_2):
-        temp = arr[index_1]
-        arr[index_1] = arr[index_2]
-        arr[index_2] = temp 
-    
-    while current_pointer <= two_pointer:
-        if arr[current_pointer] == 1:
-            pass
-        elif arr[current_pointer] == 0: 
-            if current_pointer != zero_pointer:
-                swap(current_pointer,zero_pointer)
-                zero_pointer+=1 
-                current_pointer-=1
-        else: 
-            if current_pointer != two_pointer: 
-                swap(current_pointer,two_pointer)
-                two_pointer-=1
-                current_pointer-=1 
-        current_pointer+=1  
-    return arr 
+        while(arr[two_pointer]==2):
+            two_pointer-=1  
+        while(arr[zero_pointer]==0):
+            zero_pointer+=1 
+            
+        current_pointer = zero_pointer
+            
+        def swap(index_1, index_2):
+            temp = arr[index_1]
+            arr[index_1] = arr[index_2]
+            arr[index_2] = temp 
+        
+        while current_pointer <= two_pointer:
+            if arr[current_pointer] == 1:
+                pass
+            elif arr[current_pointer] == 0: 
+                if current_pointer != zero_pointer:
+                    swap(current_pointer,zero_pointer)
+                    zero_pointer+=1 
+                    current_pointer-=1
+            elif arr[current_pointer] == 2: 
+                if current_pointer != two_pointer: 
+                    swap(current_pointer,two_pointer)
+                    two_pointer-=1
+                    current_pointer-=1 
+            else: 
+                raise ValueError("Array must contain only 0s, 1s and 2s") 
+            current_pointer+=1  
+        return arr
+    else:
+         raise ValueError("Invalid Input")
 
 print(sort_012([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])) 
 def test_function(test_case):
@@ -43,3 +48,11 @@ def test_function(test_case):
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]) 
+#edge cases  
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3])  
+test_function([])  
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, None, 1, 1, 2, 2, 2, 2, 2, 2, 2]) 
+
+
